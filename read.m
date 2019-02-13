5f881ab3c91baca9915bfbf1bbce5f47c5a3963c
tomasbrincil <me@tomasbrincil.cz>
2019-02-13 01:07:26 +0100

    figs

--- read.m
+++ read.m
@@ -28,13 +28,14 @@ end
 
 tt=T/s:T/s:(T*500);
 
+
+%% 1 - Fourierova transformace
 figure(1);
-subplot(3,1,1);
+subplot(2,1,1);
 plot(tt,y);
 title('IQ obálka signálu');
 ylabel("normalizovaná amplituda [-]");
 xlabel("èas [s]");
-
 
 n = length(y);
 X = fft(y);
@@ -42,15 +43,76 @@ Y = fftshift(X);
 fshift = (-n/2:n/2-1)*(fs*s/n);
 powershift = abs(Y).^2/n;
 
-subplot(3,1,2);
-plot(fshift,powershift);
+a = length(fshift);
+b = a/2;
+fshift_half = fshift(b+1:a);
+powershift_half = powershift(b+1:a);
+
+subplot(2,1,2);
+plot(fshift_half,powershift_half);
 title('FFT - Fourierova transformace - výkonová hustota spektra');
 ylabel("energie [-]");
 xlabel("frekvence [Hz]");
 hu = fs/2;
 %axis([0 hu 0 0]);
 
-yh=hilbert(y);
 
-subplot(3,1,3);
+%% 2 - Hilbertova transformace
+figure(2);
+subplot(2,1,1);
+plot(tt,y);
+title('IQ obálka signálu');
+ylabel("normalizovaná amplituda [-]");
+xlabel("èas [s]");
+subplot(2,1,2);
+yh=hilbert(y);
 plot(tt,yh);
+title('Hilbertova transformace');
+ylabel("normalizovaná amplituda [-]");
+xlabel("èas [s]");
+
+%% 3 - Durbinova metoda
+
+
+%% 4 - Burgova metoda
+figure(4);
+subplot(2,1,1);
+plot(powershift_half);
+title('FFT - Fourierova transformace - výkonová hustota spektra');
+ylabel("energie [-]");
+xlabel("frekvence [Hz]");
+subplot(2,1,2);
+ypb=pburg(powershift_half,16);
+plot(ypb);
+title('Burgova metoda - autoregrese modelu spektra');
+ylabel("energie [-]");
+xlabel("frekvence [Hz]");
+
+%% 5 - Pronyho metoda
+
+%% 6 - Metoda MUSIC
+figure(5);
+subplot(2,1,1);
+plot(tt,y);
+title('IQ obálka signálu');
+ylabel("normalizovaná amplituda [-]");
+xlabel("èas [s]");
+subplot(2,1,2);
+pmusic(y,64);
+title('Metoda MUSIC - výkonová hustota spektra');
+ylabel("energie [-]");
+xlabel("frekvence [Hz]");
+%% 7 - Metoda ESPRIT
+
+%% 8 - Metoda typu waterfall
+figure(7);
+subplot(2,1,1);
+plot(tt,y);
+title('IQ obálka signálu');
+ylabel("normalizovaná amplituda [-]");
+xlabel("èas [s]");
+subplot(2,1,2);
+spectrogram(y,2,1,100,fs,'yaxis');
+title('Spektrogram');
+xlabel("poèet vzorkù [-]");
+ylabel("frekvence [Hz]");
