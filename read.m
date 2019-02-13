31303722fb12215f3f7d1d332f6903244be70945
tomasbrincil <me@tomasbrincil.cz>
2019-02-13 17:00:14 +0100

    another source

--- read.m
+++ read.m
@@ -1,32 +1,11 @@
 clear all
 close all
-
-I = [];
-Q = [];
-y = [];
-
 s = 1;
-fs = 50000;
-%fs = 1;
-T = 1 / fs;
-t = T/s:T/s:T;
-
-file = './src/2FSK KV/13.4199MHz_20161109_122803.binIQ';
-
-fileID = fopen(file, 'r', 'b');
-i = dir(file);
-%s.bytes
-ALL = fread(fileID, 10000, 'uint8');
-
-for i = 1:500
-     y1=(ALL(i*2-1)-128)/128*cos(2*pi); % inphase component
-     y2=(ALL(i*2)-128)/128*sin(2*pi) ;% Quadrature component
-     I=[I y1]; % inphase signal vector%     
-     Q=[Q y2]; %quadrature signal vector
-     y=[y y1+y2]; % modulated signal vector
-end
-
-tt=T/s:T/s:(T*500);
+%fs = 19200;
+[y, fs] = audioread('./src/F1B.WAV');
+n = length(y);
+dt = 1 / fs;
+tt = 0:dt:n*dt-dt;
 
 
 %% 1 - Fourierova transformace
@@ -77,21 +56,23 @@ xlabel("èas [s]");
 %% 4 - Burgova metoda
 figure(4);
 subplot(2,1,1);
-plot(powershift_half);
+plot(fshift_half,powershift_half);
 title('FFT - Fourierova transformace - výkonová hustota spektra');
 ylabel("energie [-]");
 xlabel("frekvence [Hz]");
 subplot(2,1,2);
-ypb=pburg(powershift_half,16);
+ypb=pburg(y,16);
 plot(ypb);
 title('Burgova metoda - autoregrese modelu spektra');
 ylabel("energie [-]");
+xticks([10 20 30 40 50 60 70 80 90 100 110 120 130 140]);
+xticklabels([0 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000]);
 xlabel("frekvence [Hz]");
 
 %% 5 - Pronyho metoda
 
 %% 6 - Metoda MUSIC
-figure(5);
+figure(6);
 subplot(2,1,1);
 plot(tt,y);
 title('IQ obálka signálu');
@@ -101,6 +82,8 @@ subplot(2,1,2);
 pmusic(y,64);
 title('Metoda MUSIC - výkonová hustota spektra');
 ylabel("energie [-]");
+xticks([0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1]);
+xticklabels([0 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000]);
 xlabel("frekvence [Hz]");
 %% 7 - Metoda ESPRIT
 
@@ -112,7 +95,10 @@ title('IQ obálka signálu');
 ylabel("normalizovaná amplituda [-]");
 xlabel("èas [s]");
 subplot(2,1,2);
-spectrogram(y,2,1,100,fs,'yaxis');
+st=0:0.001:2;
+sx=chirp(st,100,1,200,'quadratic');
+spectrogram(y,64,63,512,fs,'yaxis');
+%spectrogram(y,1,1,1000,fs,'yaxis');
 title('Spektrogram');
-xlabel("poèet vzorkù [-]");
-ylabel("frekvence [Hz]");
+xlabel("èas [s]");
+ylabel("frekvence [kHz]");
